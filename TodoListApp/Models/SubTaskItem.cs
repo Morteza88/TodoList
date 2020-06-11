88eaa7864b0c6d3600689c93be07cd5103318012
morteza <morteza88@live.com>
2020-06-11 12:32:47 +0430

    Add init migration and update database

--- TodoListApp/Models/SubTaskItem.cs
+++ TodoListApp/Models/SubTaskItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,9 @@ namespace TodoListApp.Models
 {
     public class SubTaskItem
     {
+        [Key]
         public int ID { get; set; }
         public string Name { get; set; }
-        public Task Task { get; set; }
+        public TaskItem ParentTask { get; set; }
     }
 }
--- TodoListApp/Models/TaskItem.cs
+++ TodoListApp/Models/TaskItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,12 @@ namespace TodoListApp.Models
 {
     public class TaskItem
     {
+        [Key]
         public int ID { get; set; }
         public string Name { get; set; }
         public DateTime DueDate { get; set; }
         public string Priority { get; set; }
         public string Description { get; set; }
-        public ICollection<SubTaskItem> SubTaskItems { get; set; }
+        public ICollection<SubTaskItem> SubTasks { get; set; }
     }
 }
