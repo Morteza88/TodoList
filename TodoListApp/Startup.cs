30d306eae9d740dfacf41964918dccf1052b08d1
morteza <morteza88@live.com>
2020-06-11 11:55:55 +0430

    Add Database confige

--- TodoListApp/Startup.cs
+++ TodoListApp/Startup.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TodoListApp.Data;
 
 namespace TodoListApp
 {
@@ -25,6 +27,10 @@ namespace TodoListApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoListContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
